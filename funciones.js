b4584379ced90b8b87b2c8e8e998d8a3dc0d9420
Antonella Savelski <antonellasavelski@gmail.com>
2022-05-25 12:43:34 -0300

    casa

--- funciones.js
+++ funciones.js
@@ -1,11 +1,23 @@
 let arrayTareas=[];
 
 function guardarTarea() {
-    let tarea = document.getElementById("tarea").value
+    let tarea={
+        titulo: document.getElementById("item").value,
+        estado: false
+    } 
     arrayTareas.push(tarea)
 
     const node = document.createElement("li");
-    const textnode = document.createTextNode(tarea);
-    node.appendChild(textnode);
-    document.getElementById("lista").appendChild(node);
+    node.innerHTML += `<input type="checkbox"> ${tarea.titulo}</input>`
+    if (tarea.estado=false){
+        node.innerHTML += `style:"text-decoration";"line-through")`;
     }
+    document.getElementById("lista").appendChild(node);
+}
+function tachar(){
+        const tarea = document.getElementById("item").value
+        console.log(tarea)
+        if (tarea.checked){
+             $todoText.css("text-decoration","line-through");
+        }      
+}
