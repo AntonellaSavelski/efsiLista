2e7f575675b20ee403e294528680853ccfef335b
Antonella Savelski <antonellasavelski@gmail.com>
2022-05-26 18:12:55 -0300

    funciona

--- funciones.js
+++ funciones.js
@@ -1,26 +1,37 @@
 let arrayTareas=[];
+let num = 0;
 
 function guardarTarea() {
-    let tarea= document.getElementById("item").value;
+    let tarea={
+        id: num,
+        titulo: document.getElementById("item").value,
+        estado: false
+    }
     arrayTareas.push(tarea)
+    
+    let ul = document.getElementById("lista")
+    ul.innerHTML += `<li><input type="checkbox" id="${num}"onclick="done(id)"> ${tarea.titulo}</input></li>`
 
-    const node = document.createElement("li");
-    node.innerHTML += `<input type="checkbox" id="check" onclick="tachar()"> ${tarea}</input>`
-
-    document.getElementById("lista").appendChild(node);
+    num++;
+}
+let done=(id)=>{
+    arrayTareas[id].estado = true;
+    tachar();
 }
 function tachar(){
-    const c = document.getElementById("check").value
-    console.log(c);
-
-    const tarea = document.getElementById("item").value
-    console.log(tarea)
-    /*if (tarea.checked){
-        document.getElementById("item").style.textDecoration='line-through';
-    }*/
-
-    if(c === "on") {
-        document.getElementById("item").style.textDecoration='line-through';
-       
+    let ul=document.getElementById("lista")
+    document.getElementById("lista").innerHTML = "";
+    for(let i = 0; i<arrayTareas.length; i++){
+        if(arrayTareas[i].estado){
+            ul.innerHTML+=`<div class="checked">
+            <li><input type="checkbox" id="${arrayTareas[i].id}" checked disabled="disabled" onclick=done(id)> ${arrayTareas[i].titulo}</input></li>
+            </div>`
+        }else{
+            ul.innerHTML+=`<div class="notchecked">
+            <li><input type="checkbox" id="${arrayTareas[i].id}" onclick=done(id)><h10> ${arrayTareas[i].titulo} </h10>
+            </input></li>
+            </div>`
+        }
     }
 }
+
